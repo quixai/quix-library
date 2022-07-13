40a51bd06f185b32c06b6b33593a8feaca20340e
Steve R <steve@quix.ai>
2022-07-13 15:47:53 +0100

    Add http quick start

---
+++ python/sources/QuickStart/README.md
@@ -0,0 +1,20 @@
+# Python quick start - Http source
+
+This is a quick start Python project.
+
+It will send one timestamp containing two parameters into Quix.
+
+## Environment variables
+
+The code sample uses the following environment variables:
+
+- **output**: This is the output topic for quick start data.
+
+## Docs
+Check out the [SDK docs](https://quix.ai/docs/sdk/introduction.html) for detailed usage guidance.
+
+## How to run
+Create a [Quix](https://portal.platform.quix.ai/self-sign-up?xlink=github) account to edit or deploy this application without a local environment setup.
+
+Alternatively, you can learn how to set up your local environment [here](https://quix.ai/docs/sdk/python-setup.html).
+
---
+++ python/sources/QuickStart/main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+url = "https://writer-{placeholder:workspaceId}.{placeholder:environment.subdomain}.quix.ai/topics/{placeholder:output}/streams/quick-start/parameters/data"
+
+# send one timestamp with two parameters
+payload = json.dumps({
+  "timestamps": [
+    1652956783
+  ],
+  "numericValues": {
+    "temperature": [
+      24.4
+    ],
+    "humidity": [
+      61
+    ]
+  }
+})
+# configure the request headers
+headers = {
+  'Authorization': 'bearer sdk-c35ea65a30e74116a9bdfb0291ea18e0',
+  'Content-Type': 'application/json'
+}
+
+# send the POST request and get the response
+response = requests.request("POST", url, headers=headers, data=payload)
+
+# a response of 200 means the data was sent
+print("{}:{}".format(response.status_code, response.text))
