92fc1b4c10f6ece7a2704e18542b49cf77bedca9
Steve R <steve@quix.ai>
2022-07-14 10:24:55 +0100

    update with Tomas suggestions

--- python/sources/QuickStart/main.py
+++ python/sources/QuickStart/main.py
@@ -1,30 +1,44 @@
+from datetime import datetime
 import requests
 import json
+import time
+import random
 
 url = "https://writer-{placeholder:workspaceId}.{placeholder:environment.subdomain}.quix.ai/topics/{placeholder:output}/streams/quick-start/parameters/data"
+token = "bearer {placeholder:token}"
 
-# send one timestamp with two parameters
-payload = json.dumps({
-  "timestamps": [
-    1652956783
-  ],
-  "numericValues": {
-    "temperature": [
-      24.4
+
+while True:
+    date = datetime.utcnow()
+    utc_time = time.time_ns()
+    temperature = random.randint(0, 50)
+    humidity = random.randint(0, 50)
+    print("Timestamp:{} Temp:{} Humidity:{}".format(utc_time, temperature, humidity))
+
+    # send one timestamp with two parameters
+    payload = json.dumps({
+    "timestamps": [
+        utc_time
     ],
-    "humidity": [
-      61
-    ]
-  }
-})
-# configure the request headers
-headers = {
-  'Authorization': 'bearer {placeholder:token}',
-  'Content-Type': 'application/json'
-}
+    "numericValues": {
+        "temperature": [
+        temperature
+        ],
+        "humidity": [
+        humidity
+        ]
+    }
+    })
+    # configure the request headers
+    headers = {
+    'Authorization': token,
+    'Content-Type': 'application/json'
+    }
 
-# send the POST request and get the response
-response = requests.request("POST", url, headers=headers, data=payload)
+    # send the POST request and get the response
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-# a response of 200 means the data was sent
-print("{}:{}".format(response.status_code, response.text))
+    # a response of 200 means the data was sent
+    print("Response is {}".format(response.status_code))
+
+    time.sleep(1)
